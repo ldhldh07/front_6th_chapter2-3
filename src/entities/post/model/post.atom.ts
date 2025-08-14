928f9230dbd9fc3075202a742102eb6fff36aa09
ldhldh07 <dhlim07@naver.com>
2025-08-15 03:08:13 +0900

    refactor: 기존 jotai로 관리하던 서버 상태 삭제

--- src/entities/post/model/post.atom.ts
+++ src/entities/post/model/post.atom.ts
@@ -2,9 +2,5 @@ import { atom } from "jotai";
 
 import { Post } from "./post.types";
 
-export const postsAtom = atom<Post[]>([]);
-export const totalPostAtom = atom<number>(0);
-export const isPostsLoadingAtom = atom<boolean>(false);
-
 export const selectedPostAtom = atom<Post | null>(null);
 export const isPostDetailDialogOpenAtom = atom<boolean>(false);
--- src/entities/post/model/post.hook.ts
+++ src/entities/post/model/post.hook.ts
@@ -1,40 +1,15 @@
 import { useAtom } from "jotai";
 
-import {
-  isPostDetailDialogOpenAtom,
-  isPostsLoadingAtom,
-  postsAtom,
-  selectedPostAtom,
-  totalPostAtom,
-} from "./post.atom";
-
-import type { Post } from "./post.types";
+import { isPostDetailDialogOpenAtom, selectedPostAtom } from "./post.atom";
 
 export const usePosts = () => {
-  const [posts, setPosts] = useAtom(postsAtom);
-  const [total, setTotal] = useAtom(totalPostAtom);
-  const [isLoading, setIsLoading] = useAtom(isPostsLoadingAtom);
   const [selectedPost, setSelectedPost] = useAtom(selectedPostAtom);
   const [isDetailOpen, setIsDetailOpen] = useAtom(isPostDetailDialogOpenAtom);
 
-  const appendPost = (newPost: Post) => setPosts((prevPosts) => [newPost, ...prevPosts]);
-  const changePost = (newPost: Post) =>
-    setPosts((prevPosts) => prevPosts.map((prevPost) => (prevPost.id === newPost.id ? newPost : prevPost)));
-  const removePost = (postId: number) => setPosts((prev) => prev.filter((prevPost) => prevPost.id !== postId));
-
   return {
-    posts,
-    total,
-    isLoading,
     selectedPost,
     isDetailOpen,
-    setPosts,
-    setTotal,
-    setIsLoading,
     setSelectedPost,
     setIsDetailOpen,
-    appendPost,
-    changePost,
-    removePost,
   };
 };
